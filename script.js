6a46603ab29b8affcedbac66f96a1b95e6dcff07
NokibulAlam <nokibul.alam02@gmail.com>
2022-03-28 16:40:23 +0600

    Winner added to the game

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 'use strict';
 
 // Selecting Elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
-const btnHold = document.querySelector('.btn--Hold');
+const btnHold = document.querySelector('.btn--hold');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 
@@ -19,22 +21,45 @@ const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
+const switchPlayer = function() {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+}
+
+
 // Rolling dice functionality
 btnRoll.addEventListener('click', function(){
+  // 1. Generate Random dice roll
   const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
-
+// 2. Display dice
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
-
+//3. Check for rolled 1
   if (dice !== 1){
+    // Add dice to current score
     currentScore += dice;
     document.getElementById(`current--${activePlayer}`).textContent = currentScore;
   }
   else {
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
+    // Switch to next player
+    switchPlayer();
   }
+});
 
+btnHold.addEventListener('click', function() {
+  // 1. Add current score to active playe's score
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+ // 2. Check if the score >= 100
+ // Finish the game
+ if (scores[activePlayer] >= 100){
+   document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+ }
+ else {
+   //Swtich the player
+   switchPlayer();
+ }
 });
