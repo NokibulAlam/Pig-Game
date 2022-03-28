8bc0c93e8f913fe1c10068a106b792fa522ba6c4
NokibulAlam <nokibul.alam02@gmail.com>
2022-03-29 01:44:30 +0600

    game ended

--- script.js
+++ script.js
@@ -12,15 +12,28 @@ const btnHold = document.querySelector('.btn--hold');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 
-//Starting Conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, playing, currentScore, activePlayer;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function(){
+  //Starting Conditions
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+}
+
+init();
 
 const switchPlayer = function() {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -58,7 +71,7 @@ btnHold.addEventListener('click', function() {
     document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
     // 2. Check if the score >= 100
     // Finish the game
-    if (scores[activePlayer] >= 10){
+    if (scores[activePlayer] >= 100){
       diceEl.classList.add('hidden');
       playing = false;
       document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
@@ -70,3 +83,5 @@ btnHold.addEventListener('click', function() {
     }
   }
 });
+
+btnNew.addEventListener('click', init);
