79d93f17a8ec963df15bdbc9f07c3831741e1515
NokibulAlam <nokibul.alam02@gmail.com>
2022-03-28 15:55:47 +0600

    game logic build

--- script.js
+++ script.js
@@ -15,7 +15,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function(){
@@ -27,10 +29,12 @@ btnRoll.addEventListener('click', function(){
 
   if (dice !== 1){
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
   }
   else {
-
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
   }
 
 });
